195e7cbe4ff7d75d3c5efca5bf3a00725f3c5fef
Davy <davy@datalink.be>
2025-11-05 09:22:55 +0100

    fix: properly make body fill up the height, while keeping header and footer at the edge

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,9 +1,11 @@
 <template>
-    <AppHeader/>
-    <AppBody>
-        <slot />
-    </AppBody>
-    <AppFooter/>
+    <div class="h-screen w-screen grid grid-rows-[auto_1fr_auto]">
+        <AppHeader />
+        <AppBody>
+            <slot />
+        </AppBody>
+        <AppFooter />
+    </div>
 </template>
 <script setup lang="js">
     import AppFooter from '@/layouts/Partials/AppLayoutFooter.vue';
