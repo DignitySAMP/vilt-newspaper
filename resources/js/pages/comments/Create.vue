97be664af7a858d709e8143484dc71252af06124
Davy <davy@datalink.be>
2025-11-06 10:08:30 +0100

    refactor(comment/create): lint

--- resources/js/pages/comments/Create.vue
+++ resources/js/pages/comments/Create.vue
@@ -1,31 +1,31 @@
 <template>
 
-<div v-if="showCreateCommentBox">
-    <AppTextarea
-        label="Post a new comment"
-        name="content"
-        placeholder="Write your desired comment here..."
-        v-model="form.content"
-        :errors="form.errors.content"
-        :disabled="form.processing"
-        rows="6"
-    />
+    <div v-if="showCreateCommentBox">
+        <AppTextarea
+            label="Post a new comment"
+            name="content"
+            placeholder="Write your desired comment here..."
+            v-model="form.content"
+            :errors="form.errors.content"
+            :disabled="form.processing"
+            rows="6"
+        />
 
-    <div class="w-full flex justify-between">
-        <AppButton  name="btn_cancel_create_comment" :reverse="true" @click="showCreateCommentBox = false">
-            Cancel
-        </AppButton>
+        <div class="w-full flex justify-between">
+            <AppButton  name="btn_cancel_create_comment" :reverse="true" @click="showCreateCommentBox = false">
+                Cancel
+            </AppButton>
 
-        <AppButton  name="btn_create_comment" @click="submit">
-            Post comment
+            <AppButton  name="btn_create_comment" @click="submit">
+                Post comment
+            </AppButton>
+        </div>
+    </div>
+    <div class="w-full flex justify-end"  v-if="!showCreateCommentBox">
+        <AppButton name="btn_toggle_create_comment" @click="showCreateCommentBox = !showCreateCommentBox">
+            Compose new comment
         </AppButton>
     </div>
-</div>
-<div class="w-full flex justify-end"  v-if="!showCreateCommentBox">
-    <AppButton name="btn_toggle_create_comment" @click="showCreateCommentBox = !showCreateCommentBox">
-        Compose new comment
-    </AppButton>
-</div>
 </template>
 <script setup lang="js">
     import { ref } from 'vue';
